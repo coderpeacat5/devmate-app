7a4ec0deee452bb3a5349f1bf3c164593869ce0e
Gayatri <gayatrisingh2300@gmail.com>
2025-03-24 04:31:15 +0530

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("Hehehehehee")
-})
+// app.use("/user", (req, res) => {
+//   res.send("Hehehehehee")
+// })
 
-app.get("/user", (req, res) => {
-  res.send({"firstname" : "Gayatri", "lastname" : "Singh"})
-})
+// app.get("/user", (req, res) => {
+//   res.send({"firstname" : "Gayatri", "lastname" : "Singh"})
+// })
 
-app.post("/user", (req, res) => {
-  // saving data to db
-  res.send("Data saved to database successfully!")
-})
+// app.post("/user", (req, res) => {
+//   // saving data to db
+//   res.send("Data saved to database successfully!")
+// })
 
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully")
-})
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("User deleted successfully")
+// })
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
