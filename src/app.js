07ee46b9538696f3068f4c572d56e3bbeb87e131
Gayatri <gayatrisingh2300@gmail.com>
2025-03-25 02:44:11 +0530

    auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,22 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
-// Handle Auth Middleware for all GET, POST, ... requests
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
+app.get("/getUserData", (req, res) => {
     res.send("User Data Sent");
 });
 
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("Deleted a user");
-});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
