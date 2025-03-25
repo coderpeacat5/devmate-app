dc500d12e2d20697255467700d501aa355e48403
Gayatri <gayatrisingh2300@gmail.com>
2025-03-26 01:35:52 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ const User = require("./models/user")
 
 const app = express();
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
+
   // creating a new instance of User model
-  const user = new User({
-    firstName: "Toto",
-    lastName: "Singh",
-    emailId: "toto@mail.com",
-    password: "123"
-  })
+  const user = new User(req.body)
 
   try {
     await user.save();
@@ -19,6 +17,55 @@ app.post("/signup", async (req, res) => {
   } 
   catch (err) {
     res.status(400).send("Error saving user" + err.message)
+  }
+})
+
+// GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({emailId : userEmail})
+    if(user.length === 0) {
+      res.status(400).send("something went wrong!")
+    } else {
+      res.send(user)
+    }
+    
+  }
+  catch(err) {
+    res.status(500).send("something went wrong")
+  }
+})
+
+// Feed API - GET /feed - get all the users from the feed
+app.get("/feed" , async (req, res) => {
+  try {
+    const user = await User.find({})
+    if(user.length === 0) {
+      res.status(400).send("something went wrong!")
+    } else {
+      res.send(user)
+    }
+  }
+  catch(err) {
+    res.status(500).send("something went wrong")
+  }
+})
+
+app.get("/userId", async (req, res) => {
+  const userId = req.body.id
+  try {
+    const user = await User.findById({_id : userId})
+    if(!user) {
+      res.status(400).send("something went wrong")
+    }
+    else {
+      res.send(user)
+    }
+  }
+  catch(err) {
+    res.send(err.message)
   }
 })
 
