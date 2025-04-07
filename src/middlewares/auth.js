5b7d8ff1abebfa938eefc85197911f2ba6edcd98
Gayatri <gayatrisingh2300@gmail.com>
2025-04-08 03:23:58 +0530

    response if no token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
         // Read the token from cookie
         const { token } = req.cookies;
         if (!token) {
-            throw new Error("Token is not valid!!")
+            return res.status(401).send("Please sign in!!")
         }
 
         // Validate the token
