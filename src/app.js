99256c0243772b85e8fc3d2fd1240bdd855063e1
Gayatri <gayatrisingh2300@gmail.com>
2025-04-13 03:48:45 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(cors({
   origin : "http://localhost:5173",
   credentials : true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders : ['Content-Type']
 }))
+
 app.use(express.json())
 app.use(cookieParser())
 
