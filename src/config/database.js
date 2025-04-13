58c213c000d9e1257df2799d687e73cb4622e13a
Gayatri <gayatrisingh2300@gmail.com>
2025-04-14 03:12:40 +0530

    update DB password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://namastedev:D8CHG1wF6BaR1RFR@namastenode.mg2rr.mongodb.net/devMate"
+        "mongodb+srv://namastedev:bUk3qOiiUybqaIpj@namastenode.mg2rr.mongodb.net/devMate"
     )
 }
 
