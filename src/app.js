151067437acea7bc2f73ffb9c504c014c6e9e132
Gayatri <gayatrisingh2300@gmail.com>
2025-04-15 04:05:09 +0530

    api fix

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors({
   origin : "http://localhost:5173",
   credentials : true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }))
 
 app.use(express.json())
@@ -22,7 +21,7 @@ const userRouter = require("./routes/user")
 app.use("/", authRouter)
 app.use("/", profileRouter)
 app.use("/", requestRouter)
-app.use("/", userRouter)
+app.use("/api", userRouter)
 
 connectDB()
   .then(() => {
