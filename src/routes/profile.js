9a42c13d0a61f0d191fef24df7c472187b21a525
Gayatri <gayatrisingh2300@gmail.com>
2025-04-15 04:27:37 +0530

    route fix

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,21 +1,18 @@
-const express = require("express")
-const { userAuth } = require("../middlewares/auth");
-const { validateEditProfileData, validatePassword } = require("../utils/validation");
-const User = require("../models/user");
-const bcrypt = require("bcrypt")
-
+const express = require("express");
 const profileRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
 
-    res.send(user)
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
-  catch (err) {
-    res.status(400).send("ERROR: " + err.message)
-  }
-})
+});
 
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
@@ -38,33 +35,4 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   }
 });
 
-profileRouter.patch("/profile/password", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-
-    const { oldPassword, newPassword } = req.body;
-
-    // Check if old password is correct
-    const isMatch = await bcrypt.compare(oldPassword, user.password)
-    if (!isMatch) {
-      throw new Error("Old password is incorrect")
-    }
-
-    // Validate new password
-    if (!validatePassword(newPassword)) {
-      throw new Error("Please enter a strong password")
-    }
-
-    // Update new password
-    user.password = await bcrypt.hash(newPassword, 10);
-    await user.save();
-
-    res.send("Password changed successfully!!")
-
-  }
-  catch (err) {
-    res.status(400).send("ERROR: " + err.message)
-  }
-})
-
-module.exports = profileRouter; 
+module.exports = profileRouter;
