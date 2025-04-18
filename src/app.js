5b07de1d90c40a5167f6199dc543673a60a4c46c
Gayatri <gayatrisingh2300@gmail.com>
2025-04-18 22:24:54 +0530

    add origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const app = express();
 
 app.use(cors({
-  origin : "http://localhost:5173",
+  origin : ["http://localhost:5173", "http://16.170.206.18"],
   credentials : true,
 }))
 
