4d0f61ee0f2d814b94b92443dbc52e8aec684cc9
Gayatri <gayatrisingh2300@gmail.com>
2025-04-19 05:53:51 +0530

    moved dotenv to top

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const connectDB = require('./config/database')
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-require('dotenv').config()
 
 const app = express();
 
